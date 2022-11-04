b733136b8d49107fc66973b6c5033c392bcdece5
SnowHawkeye <hadriensalem@gmail.com>
2022-11-04 16:55:54 +0100

    Continuation of R tutorial (transformation)

--- src/main/r/tutorials/r4ds_transformation.R
+++ src/main/r/tutorials/r4ds_transformation.R
@@ -2,3 +2,19 @@ library(ggplot2)
 library(dplyr)
 library(nycflights13)
 
+filter(flights, year == 2013)
+arrange(flights, desc(dep_delay)) # arrange sorts the data
+
+df <- tibble(x = c(5, 2, NA))
+arrange(df, desc(is.na(x)))
+
+# Select columns by name
+select(flights, year, month, day)
+
+# Select all columns between year and day (inclusive)
+select(flights, year:day)
+
+# Select all columns except those from year to day (inclusive)
+select(flights, -(year:day))
+
+# The help for the select() function presents helper functions such as "starts_with()" and more
